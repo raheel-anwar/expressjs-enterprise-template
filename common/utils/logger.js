7ff4a0bddada96d7f5fe51ed6ef54cef053320a0
Raheel Anwar <raheelanwar74@gmail.com>
2019-06-28 11:08:12 +0500

    updated logger

--- common/utils/logger.js
+++ common/utils/logger.js
@@ -4,23 +4,23 @@ const colors = require('colors/safe');
 module.exports = {
 
     log(message) {
-        console.log(colors.white(`${moment()}: INFO: ${message}`));
+        console.log(`${colors.green(moment().toString())}: ${colors.cyan('INFO')}: ${message}`);
     },
     
     warn(message) {
-        console.log(colors.yellow(`${moment()}: WARN: ${message}`));
+        console.log(`${colors.green(moment().toString())}: ${colors.yellow('WARN')}: ${message}`);
     },
 
     error(message) {
-        console.log(colors.red(`${moment()}: ERROR: ${message}`));
+        console.log(`${colors.green(moment().toString())}: ${colors.red('ERROR')}: ${message}`);
     },
 
     fatal(message) {
-        console.log(colors.red(`${moment()}: FATAL: ${message}`));
+        console.log(`${colors.green(moment().toString())}: ${colors.red('FATAL')}: ${message}`);
     },
 
     debug(message) {
-        console.log(colors.white(`${moment()}: DEBUG: ${message}`));
+        console.log(`${colors.green(moment().toString())}: ${colors.yellow('DEBUG')}: ${message}`);
     }
 
 }
